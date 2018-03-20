642072f12140d27d93b9929a2355717161dc36f0
Lukasz Marciniak <lmarnavig@gmail.com>
2018-03-20 20:53:16 +0100

    [ADD] server functionality + rest + dynamic creation to specific elements

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+const fs = require('fs');
 
 // configure app to use bodyParser()
 // get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 3001;    //set port
+let rawdata = fs.readFileSync(path.join(__dirname+'/resources/json/data.json'));
+let jsonObj = JSON.parse(rawdata); 
 
-// resources css
+// resources 
 app.use('/static', express.static(path.join(__dirname, 'resources')))
 
 app.get('/',function(req,res){
@@ -18,7 +20,37 @@ app.get('/',function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
+// get all data from json file
+app.get('/getAllData', function(req, res){  
+    res.setHeader('Content-Type', 'application/json');
+    res.send(rawdata);
+});
+
+app.post('/addItemToCart', function(req, res){
+    var id = req.body.itemId;
+    var itemData = jsonObj[id-1];
+    itemData = JSON.stringify(itemData, null, 2); 
+    fs.appendFile(path.join(__dirname+'/resources/json/cart.json'), itemData + ',');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(itemData);
+});
+
+app.post('/clearCart', function(req, res){
+    fs.writeFile(path.join(__dirname+'/resources/json/cart.json'), '');
+});
+
+app.get('/getCartItems', function(req, res) {
+    let rawdataCart = fs.readFileSync(path.join(__dirname+'/resources/json/cart.json'));
+    res.setHeader('Content-Type', 'application/json');
+    res.send(rawdataCart);
+});
 
 // start server
-app.listen(port);
-console.log('Magic happens on port ' + port);
+var server = app.listen(3000, '0.0.0.0', () => {
+  var host = server.address().address;
+  var port = server.address().port;
+  
+  console.log('Server is running on http://%s:%s', host, port);
+}).on('error', (err) => {
+    console.log("error", err);
+});
--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
     
+//    Initialize spots
+    createSpots();
+    
+    
 //    Cart item on hover show remove link
     $('.cart-item').hover(
       function() {
@@ -24,73 +28,95 @@ $(document).ready(function() {
         }
     });
     
-//    How many items in cart and update required fields
-//    TODO change this to function
+//    Count items and pirces and update required fileds
+    countItems();
     countPrice();
     
-//    Count cart prices and update required fileds
-//    TODO change this to function
-    let res = 0;
-    $('.cart-item-price').each(function() {
-        res += parseFloat($(this).html().trim().substring(1));
-    });
-    $('.cart-price-all').html('$' + res);
-    
 //    Clear cart when button clicked
-    $('#clearCart').on('click', function(){
+    $('#btn-clear').on('click', function(){
         $('.cart-items').empty();
-        $('.small-spots .btn').html('test');
+        $.post('/clearCart');
+        createSpots();
 //        TODO when invoke functions to calculate priceses and number of items
     });
     
-    $('.small-spots .btn').on('click', function() {
+    $('#spotsContainer').on('click','button', function() {
         let btnText = $(this).html();
-        const imgUrl = $(this).parents('.small-spots').children('img').attr('src');
-        const title = $(this).parents('.small-spots').children('p').html();
+        let btnId = $(this).attr('id');
+        let id = btnId.substring(4);
         if(btnText === 'IN CART') {
             return false;
         }
         else {
             $(this).html('IN CART');
-            btnText = btnText.substring(1)
-            console.log(btnText);
-            console.log(imgUrl);
-            console.log(title);
-            addNewElementToCart(btnText, imgUrl, title);
+            
+            $.post('/addItemToCart', {"itemId": id}, function(data){
+                let newElem = '';
+                const mouseOver = "$(this).find('small').css('visibility', 'visible');";
+                const mouseLeave = "$(this).find('small').css('visibility', 'hidden');";
+                newElem += `<div class="dropdown-divider"></div>
+                            <div class="cart-item" onmouseover="${mouseOver}" onmouseleave="${mouseLeave}">
+                            <img src="${data.image}" alt="">
+                            <div class="cart-item-title align-middle">
+                            <div>${data.title}</div>
+                            <small class="form-text text-muted cart-item-remove">
+                            <a href="#">Remove</a>
+                            </small></div>
+                            <div class="cart-item-price">$${data.priceBtn}</div></div>`;
+    
+                $('.cart-items').append(newElem);
+            });
+            
+            
         }
     });
     
     $('.cart-items').bind("DOMSubtreeModified",function(){
         countPrice();
+        countItems();
     });
     
 });
 
-function addNewElementToCart(price, imgUrl, title) {
-    let newElem = '';
-    
-    newElem += `<div class="dropdown-divider"></div>
-                <div class="cart-item">
-                    <img src="${imgUrl}" alt="">
-                    <div class="cart-item-title align-middle">
-                        <div>${title}</div>
-                        <small class="form-text text-muted cart-item-remove">
-                            <a href="#">Remove</a>
-                        </small>
-                    </div>
-                    <div class="cart-item-price">$${price}</div>
-                </div>`;
-    
-    $('.cart-items').append(newElem);
+function countPrice(){
+    let res = 0;
+    $('.cart-item-price').each(function() {
+        res += parseFloat($(this).html().trim().substring(1));
+    });
+    $('.cart-price-all').html('$' + res);
 }
 
 function countItems(){
-    
-}
-
-function countPrice(){
     const items = $('.cart-item').length;
     $('.text-cart').html(items);
     const str = items === 1 ? 'item' : 'items';
     $('.cart-item-all').html(items + ' ' + str + ' in cart');
+}
+
+function createSpots() {
+    $.get('/getAllData', function(data){
+        let newElem = '';
+        
+        $.each(data, (key, val) => {
+            
+            if(key === 0) newElem += `<div class="col-2 offset-1">`;
+            else newElem += `<div class="col-2">`;
+            
+            newElem += `<div class="small-spots" id="spot-${val.id}">
+                        <img src="${val.image}" />
+                        <p class="text-spots">${val.title}</p>
+                        <div class="price-tags d-flex justify-content-end">`;
+            
+            if(val.discount !== false) newElem += `<span class="badge badge-success">${val.discount}</span>`;
+            
+            if(val.priceBtn === 'OWNED') newElem += `<button class="btn" id="btn-owned" disabled>`;
+            else newElem += `<button class="btn" id="btn-${val.id}">$`;
+            
+            newElem += `${val.priceBtn}</button></div></div></div>`;
+            
+        });
+        
+        $("#spotsContainer").html(newElem);
+        
+    });
 }
